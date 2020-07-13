ce64b6b13089ccda10d2d84a9c000d5b566fb03a
bonghyunyang <bonghyunyang2@gmail.com>
2020-07-13 14:47:37 +0900

    최종완성

---
+++ ToDoList/my_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'yna%m4!-1kg7ctmk8nj!wfi&#spwo7k=*b1(h!*c@s1l=-@0$)'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
